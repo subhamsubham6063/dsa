f672bad09aceab683bc4ef6cfc83a72b43ccdb02
subhamsubham6063 <132640821+subhamsubham6063@users.noreply.github.com>
2025-12-09 14:25:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+         vector<int> ans;
+         unordered_map<int,int>mp;
+         
+        for(auto it :arr){
+            mp[it]++;
+        }
+        
+        for(auto it :mp){
+           if(it.second == 2){
+               ans.push_back(it.first);
+           }
+        }
+        
+        return ans;
+    }
+};
